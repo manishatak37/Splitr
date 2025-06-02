b4cb2af858266a64b7b0bb2e783fd9f746dd4d0e
manishatak37 <takmanisha67@gmail.com>
2025-06-02 17:20:49 +0530

    Attached the inngest for autmatic workflows

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,10 +10,19 @@ import React from "react";
 import { useStoreUser } from "@/hooks/use-store-user";
 import Link from "next/link"
 import Image from "next/image"; // ✅
-import {BarLoader} from "react-spinners"
+import { BarLoader } from "react-spinners"
+import { usePathname } from "next/navigation";
+import { Unauthenticated } from "convex/react";
+import { Authenticated } from "convex/react";
+import { Button } from "@/components/ui/button"
+import { SignIn } from "@clerk/nextjs";
+import { Ghost, LayoutDashboard } from "lucide-react";
+
 
 const Header = () => {
   const { isloading } = useStoreUser()
+  const path = usePathname();
+
   return (
     <header className="fixed top-0 w-full border-b  bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -26,7 +35,40 @@ const Header = () => {
             className="h-11 w-auto object-contain"
           />
         </Link>
+        {path === '/' && (
+          <div className="hidden md:flex items-center gap-6">
+            <Link href="#features" className="text-sm font-medium hover:text-green-600 transition">Features</Link>
+            <Link href="#how-it-works" className="text-sm font-medium hover:text-green-600 transition">How it Works</Link>
+          </div>
+        )}
+
+        <div className="flex items-center gap-4">
+          <Authenticated>
+            <Link href="/dashboard">
+              <Button variant="outline" className="hidden md:inline-flex items-center gap-2 hover:text-green-600 hover:border-green-600 transition">
+              <LayoutDashboard className="h-4 w-4" />
+              Dashboard
+              </Button>
+              <Button variant={Ghost} className="md:hidden w-10 h-10 p-0">
+              <LayoutDashboard className="h-4 w-4" />
+              </Button>
+            </Link>
+            <UserButton />
+          </Authenticated>
+          <Unauthenticated>
+            <SignInButton>
+              <Button variant={"ghost"}>Sign In</Button>
+            </SignInButton>
+
+            <SignUpButton>
+              <Button className="bg-green-600 hover:bg-green-700 border-none">
+                Get Started
+              </Button>
+            </SignUpButton>
+          </Unauthenticated>
+        </div>
       </nav>
+
       {isloading && <BarLoader width={"100%"} color="#36d7b7" />}
 
     </header>
